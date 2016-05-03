07a664a04fcccbb35946be8b2edae0c92ec0ff69
Christine Rohacz <crohacz@gmail.com>
2016-05-02 17:08:32 -0700

    Updated background geolocation

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -11,41 +11,6 @@ module.exports = [
         "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
         "id": "cordova-plugin-geolocation.PositionError",
         "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-background-fetch/www/BackgroundFetch.js",
-        "id": "cordova-plugin-background-fetch.BackgroundFetch",
-        "clobbers": [
-            "window.BackgroundFetch"
-        ]
-    },
-    {
-        "file": "plugins/com.transistorsoft.cordova.background-geolocation/www/BackgroundGeolocation.js",
-        "id": "com.transistorsoft.cordova.background-geolocation.BackgroundGeolocation",
-        "clobbers": [
-            "window.BackgroundGeolocation"
-        ]
-    },
-    {
-        "file": "plugins/org.flybuy.cordova.background-location-services/www/BackgroundLocationServices.js",
-        "id": "org.flybuy.cordova.background-location-services.BackgroundLocationServices",
-        "clobbers": [
-            "plugins.backgroundLocationServices"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
-        "id": "cordova-plugin-dialogs.notification",
-        "merges": [
-            "navigator.notification"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
-        "id": "cordova-plugin-dialogs.notification_android",
-        "merges": [
-            "navigator.notification"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -53,11 +18,7 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-geolocation": "2.2.1-dev",
-    "cordova-plugin-background-fetch": "3.0.3",
-    "com.transistorsoft.cordova.background-geolocation": "1.5.1",
-    "org.flybuy.cordova.background-location-services": "1.0.1",
-    "cordova-plugin-compat": "1.0.0",
-    "cordova-plugin-dialogs": "1.2.1"
+    "cordova-plugin-compat": "1.0.0"
 }
 // BOTTOM OF METADATA
 });
